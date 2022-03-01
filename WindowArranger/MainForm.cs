d964e436b0b135dfeb84146b8f1f18ea07b63357
Scott Stephens <stephens.js@gmail.com>
2022-03-01 10:38:07 -0600

    Switch to WinForms app so it can be Per Monitor DPI aware
    * This allows it to get the real pixel dimensions of the monitors.

---
+++ WindowArranger/MainForm.cs
@@ -0,0 +1,18 @@
+namespace WindowArranger
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object? sender, EventArgs e)
+        {
+            var p = new ConsoleProgram();
+            p.Run(new string[] { });
+        }
+    }
+}
--- WindowArranger/Program.cs
+++ WindowArranger/Program.cs
@@ -2,10 +2,16 @@
 {
     internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            var p = new ConsoleProgram();
-            p.Run(args);
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
         }
     }
 }
